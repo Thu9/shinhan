3269b5f08fbee58f7de16ec6813e465304bad24c
Thu9 <leete416@naver.com>
2024-09-05 18:19:04 +0900

    emoji Update

--- src/parts/Part1-3.tsx
+++ src/parts/Part1-3.tsx
@@ -22,43 +22,45 @@ export default function Part_1_3() {
   return (
     <div className="m-[10px]">
       <Slider {...sliderSettings}>
-        {part1_3Data.map(({ name, range, earnings, contents }, index) => (
-          <div key={index} className="pr-[10px]">
-            <BorderBox>
-              <div className="my-[20px] mx-[15px]">
-                <div className="flex items-center">
-                  <div className="w-[48px] h-[42px] flex">
-                    <img
-                      className="m-auto "
-                      src="src/assets/icons/emoji-1.png"
-                    ></img>
-                  </div>
+        {part1_3Data.map(
+          ({ name, range, earnings, contents, emoji }, index) => (
+            <div key={index} className="pr-[10px]">
+              <BorderBox>
+                <div className="my-[20px] mx-[15px]">
+                  <div className="flex items-center">
+                    <div className="w-[48px] h-[42px] flex">
+                      <img
+                        className="m-auto"
+                        src={`src/assets/icons/emoji-${emoji}.png`}
+                      ></img>
+                    </div>
 
-                  <div className="ml-[5px]">
-                    <p
-                      className="font-semibold text-[10px] text-shinhan-Blue-350 bg-shinhan-Blue-150
+                    <div className="ml-[5px]">
+                      <p
+                        className="font-semibold text-[10px] text-shinhan-Blue-350 bg-shinhan-Blue-150
                 px-[4px] w-fit h-[14px]"
-                    >
-                      {range}
-                      <span className="desc">{name}</span>
-                    </p>
+                      >
+                        {range}
+                        <span className="desc">{name}</span>
+                      </p>
 
-                    <p className="font-extrabold text-[12px] mt-[2px] text-shinhan-Blue-500">
-                      추가 소득
-                      <span className="font-[900] tracking-tight">
-                        &nbsp;+{formatWon(earnings)}원
-                      </span>
-                    </p>
+                      <p className="font-extrabold text-[12px] mt-[2px] text-shinhan-Blue-500">
+                        추가 소득
+                        <span className="font-[900] tracking-tight">
+                          &nbsp;+{formatWon(earnings)}원
+                        </span>
+                      </p>
+                    </div>
                   </div>
-                </div>
 
-                <p className="text-[12px] p-[10px] h-[149px] mt-[10px] text-left ellipsis">
-                  {contents}
-                </p>
-              </div>
-            </BorderBox>
-          </div>
-        ))}
+                  <p className="text-[12px] p-[10px] h-[149px] mt-[10px] text-left ellipsis">
+                    {contents}
+                  </p>
+                </div>
+              </BorderBox>
+            </div>
+          )
+        )}
       </Slider>
     </div>
   );
